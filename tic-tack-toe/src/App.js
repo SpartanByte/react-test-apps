71338c70ce79f50e56b658e36a224ea2ffa1975d
spartanbyte <briwar10@outlook.com>
2021-05-30 19:36:23 -0500

    setting up Cards

--- tic-tack-toe/src/App.js
+++ tic-tack-toe/src/App.js
@@ -1,8 +1,19 @@
+import React, {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
-import Icon from "./components/Icon"
+import Icon from "./components/Icon";
 
-function App() {
+//React-Toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+//Bootstrap
+import {Card, CardBody, Container, Button, Col, Row} from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+
+//9 possible position
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
--- tic-tack-toe/src/components/Icon.js
+++ tic-tack-toe/src/components/Icon.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {FaTimes, FaPen, FaRegCircle} from 'react-icons/fa';
 
+
 const Icon = ({name}) => {
     switch(name){
         case 'circle':
